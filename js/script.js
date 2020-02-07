5373c837339fc4f9404a4446138dbbd0431f60f2
Arkadiusz Zygan <a.zygan@gmail.com>
2020-02-07 10:23:52 +0100

    Add listeners

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const buttons = document.querySelector('.buttons button');
+const buttons = document.querySelector('.buttons');
 const resultDisplay = document.querySelector('.result');
 
 const clear = () => {
-
+  console.log('clear');
 }
 
 const clearAll = () => {
-  
+  console.log('clearAll');
 }
 
 const calculation = () => {
@@ -26,3 +26,18 @@ const equal = () => {
 }
 
 // listeners
+
+buttons.addEventListener('click', evt => {
+  if(evt.target.className.includes("number")){
+    number(evt.target.innerText);
+  } else if (evt.target.className.includes("operator")) {
+    operator(evt.target.innerText);
+  } else if (evt.target.className.includes("clear")) {
+    clear();
+  } else if (evt.target.className.includes("allCle")) {
+    clearAll();
+  } else if (evt.target.className.includes("equal")) {
+    equal();
+  }
+  
+});
