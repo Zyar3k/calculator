5a9f9c72e415e03f5884c81d106de2d7ca820913
Arkadiusz Zygan <a.zygan@gmail.com>
2020-02-07 10:00:37 +0100

    Add pattern for js script

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelector('.buttons button');
+const resultDisplay = document.querySelector('.result');
+
+const clear = () => {
+
+}
+
+const clearAll = () => {
+  
+}
+
+const calculation = () => {
+  
+}
+
+const number = () => {
+  
+}
+
+const operator = () => {
+  
+}
+
+const equal = () => {
+  
+}
+
+// listeners
